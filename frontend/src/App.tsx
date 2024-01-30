3293233362494acf0201cb739020ee04d680859c
etesam <etesamansari13@gmail.com>
2024-01-29 21:41:10 -0500

    :lipstick: Configured `tailwindcss`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 // import { Greet } from "../wailsjs/go/main/App";
 
 function App() {
-	return <div id="App">testing</div>;
+  return (
+    <div id="App" className="bg-blue-200">
+      testing
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./style.css";
+import "./index.css";
 
 const container = document.getElementById("root");
 
 if (container) {
-	const root = createRoot(container);
+  const root = createRoot(container);
 
-	root.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-	);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
