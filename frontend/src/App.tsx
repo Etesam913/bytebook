87d449d6fa147e6c9e9b92aa62b59b986386d259
etesam <etesamansari13@gmail.com>
2024-01-29 19:31:16 -0500

    :tada: Initialized wails project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import logo from './assets/images/logo-universal.png';
+import './App.css';
+import { Greet } from '../wailsjs/go/main/App';
+
+function App() {
+  return <div id="App"></div>;
+}
+
+export default App;
