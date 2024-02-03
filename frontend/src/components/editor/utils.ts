7cc4f0d16a27857065cb527d5194a3b0607756d4
etesam <etesamansari13@gmail.com>
2024-02-03 17:18:53 -0500

    :sparkles: Editor menu works with headers & paragraphs

---
+++ frontend/src/components/editor/utils.ts
@@ -0,0 +1,38 @@
+import { $createHeadingNode, HeadingNode } from "@lexical/rich-text";
+import { $setBlocksType } from "@lexical/selection";
+import {
+  $createParagraphNode,
+  $getSelection,
+  $isRangeSelection,
+  LexicalEditor,
+} from "lexical";
+import { EditorBlockTypes } from "../../types";
+
+/**
+ * Gets a selection and formats the blocks to `newBlockType`
+ */
+export function changeSelectedBlocksType(
+  editor: LexicalEditor,
+  newBlockType: EditorBlockTypes
+) {
+  editor.update(() => {
+    const selection = $getSelection();
+
+    if ($isRangeSelection(selection)) {
+      switch (newBlockType) {
+        case "paragraph":
+          $setBlocksType(selection, () => $createParagraphNode());
+          break;
+        case "h1":
+          $setBlocksType(selection, () => $createHeadingNode("h1"));
+          break;
+        case "h2":
+          $setBlocksType(selection, () => $createHeadingNode("h2"));
+          break;
+        case "h3":
+          $setBlocksType(selection, () => $createHeadingNode("h3"));
+          break;
+      }
+    }
+  });
+}
