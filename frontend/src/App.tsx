5cdff899dc1e4cb3bbf21c7f5fefc684dea38a30
etesam <etesamansari13@gmail.com>
2024-02-03 19:49:10 -0500

    :sparkles: Got `ol` and `ul` lists to work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { NotesEditor } from "./components/editor/note-editor";
 // import { Greet } from "../wailsjs/go/main/App";
 
 function App() {
-	return (
-		<div id="App" className="bg-blue-200">
-			<NotesEditor />
-		</div>
-	);
+  return (
+    <div id="App" className="bg-blue-200">
+      <NotesEditor />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/editor/note-editor.tsx
+++ frontend/src/components/editor/note-editor.tsx
@@ -1,66 +1,81 @@
 import { CodeNode } from "@lexical/code";
 import { LinkNode } from "@lexical/link";
 import { ListItemNode, ListNode } from "@lexical/list";
-import { TRANSFORMERS } from "@lexical/markdown";
-import { CheckListPlugin } from "@lexical/react/LexicalCheckListPlugin";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { HorizontalRuleNode } from "@lexical/react/LexicalHorizontalRuleNode";
 import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
-import { useEffect, useState } from "react";
+import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
+import { useState } from "react";
 import { EditorBlockTypes } from "../../types";
 import { Toolbar } from "./toolbar";
 import { CUSTOM_TRANSFORMERS } from "./transformers";
+import { EditorThemeClasses } from "lexical";
 
-const theme = {};
+const theme: EditorThemeClasses = {
+  list: {
+    ulDepth: ["root-ul", "ul-1"],
+    olDepth: ["root-ol", "ol-1"],
+    listitem: "root-li",
+    nested: {
+      listitem: "PlaygroundEditorTheme__nestedListItem",
+    },
+  },
+};
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
 function onError(error: Error) {
-	console.error(error);
+  console.error(error);
 }
 
 export function NotesEditor() {
-	const [currentBlockType, setCurrentBlockType] = useState<EditorBlockTypes>();
+  const [currentBlockType, setCurrentBlockType] = useState<EditorBlockTypes>();
 
-	return (
-		<LexicalComposer
-			initialConfig={{
-				namespace: "MyEditor",
-				theme,
-				onError,
-				nodes: [
-					HeadingNode,
-					QuoteNode,
-					HorizontalRuleNode,
-					CodeNode,
-					LinkNode,
-					ListNode,
-					ListItemNode,
-				],
-			}}
-		>
-			<Toolbar
-				currentBlockType={currentBlockType}
-				setCurrentBlockType={setCurrentBlockType}
-			/>
-			<div className="bg-zinc-200  p-2">
-				<RichTextPlugin
-					placeholder={null}
-					contentEditable={<ContentEditable />}
-					ErrorBoundary={LexicalErrorBoundary}
-				/>
-				<HistoryPlugin />
-				<CheckListPlugin />
-				<MarkdownShortcutPlugin transformers={CUSTOM_TRANSFORMERS} />
-			</div>
-		</LexicalComposer>
-	);
+  return (
+    <LexicalComposer
+      initialConfig={{
+        namespace: "MyEditor",
+        theme,
+        onError,
+        nodes: [
+          HeadingNode,
+          QuoteNode,
+          HorizontalRuleNode,
+          CodeNode,
+          LinkNode,
+          ListNode,
+          ListItemNode,
+        ],
+      }}
+    >
+      <Toolbar
+        currentBlockType={currentBlockType}
+        setCurrentBlockType={setCurrentBlockType}
+      />
+      <div className="bg-zinc-200  p-2">
+        {/* <OnChangePlugin
+          onChange={(e) => {
+            console.log(e);
+          }}
+        /> */}
+        <RichTextPlugin
+          placeholder={null}
+          contentEditable={<ContentEditable />}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <ListPlugin />
+        <TabIndentationPlugin />
+        <HistoryPlugin />
+
+        <MarkdownShortcutPlugin transformers={CUSTOM_TRANSFORMERS} />
+      </div>
+    </LexicalComposer>
+  );
 }
