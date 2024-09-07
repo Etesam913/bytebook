680220dc82f60ab3a14b145732d9e21b46090b71
etesam <etesamansari13@gmail.com>
2024-09-07 19:46:02 -0400

    :bug: Fixed bug with onClick on inline equation

--- frontend/src/components/inline-equation/index.tsx
+++ frontend/src/components/inline-equation/index.tsx
@@ -1,17 +1,44 @@
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useLexicalNodeSelection } from "@lexical/react/useLexicalNodeSelection";
+import { mergeRegister } from "@lexical/utils";
+import { CLICK_COMMAND, COMMAND_PRIORITY_NORMAL } from "lexical";
+import { useEffect, useRef } from "react";
+import { onClickDecoratorNodeCommand } from "../../utils/commands";
 import { KatexRenderer } from "./katex-renderer";
 
 export function InlineEquation({
 	nodeKey,
 	equation,
 }: { nodeKey: string; equation: string }) {
-	const [isSelected] = useLexicalNodeSelection(nodeKey);
+	const [editor] = useLexicalComposerContext();
+	const [isSelected, setSelected, clearSelection] =
+		useLexicalNodeSelection(nodeKey);
+	const inlineEquationRef = useRef<HTMLSpanElement>(null);
+
+	useEffect(() => {
+		return mergeRegister(
+			editor.registerCommand<MouseEvent>(
+				CLICK_COMMAND,
+				(e) => {
+					e.stopPropagation();
+					return onClickDecoratorNodeCommand(
+						e,
+						inlineEquationRef.current,
+						setSelected,
+						clearSelection,
+					);
+				},
+				COMMAND_PRIORITY_NORMAL,
+			),
+		);
+	}, []);
 
 	return (
 		<span
+			ref={inlineEquationRef}
 			className={
 				isSelected
-					? "rounded-sm outline-2 outline outline-blue-300 dark:outline-blue-500 dark:border-blue-500"
+					? "bg-blue-500 bg-opacity-35 dark:bg-blue-400 dark:bg-opacity-55"
 					: ""
 			}
 		>
