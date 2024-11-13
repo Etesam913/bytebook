f8d78451c95ea274144e1506d640aa649f6ba89e
etesam <etesamansari13@gmail.com>
2024-11-12 20:08:30 -0500

    :lipstick: Added settings option to change color theme

--- frontend/src/components/settings-dialog/appearance-page.tsx
+++ frontend/src/components/settings-dialog/appearance-page.tsx
@@ -1,40 +1,80 @@
-import { useSetAtom } from "jotai/react";
+import { useAtom } from "jotai/react";
 import { darkModeAtom } from "../../atoms";
+import { cn } from "../../utils/string-formatting";
 import { SettingsRow } from "./settings-row";
 
+function DarkModeButton({
+	label,
+	imgSrc,
+	imgAlt,
+	onClick,
+	isActive,
+}: {
+	label: string;
+	imgSrc: string;
+	imgAlt: string;
+	onClick: () => void;
+	isActive: boolean;
+}) {
+	return (
+		<button type="button" onClick={onClick}>
+			<p
+				className={cn(
+					"text-sm text-zinc-500 dark:text-zinc-400",
+					isActive && "text-zinc-950 dark:text-zinc-100 ",
+				)}
+			>
+				{label}
+			</p>
+			<img
+				draggable="false"
+				className={cn(
+					"border-[3px] rounded-md p-1 border-zinc-200 dark:border-zinc-750",
+					isActive && "border-blue-400 dark:border-blue-500",
+				)}
+				src={imgSrc}
+				alt={imgAlt}
+			/>
+		</button>
+	);
+}
+
 export function AppearancePage() {
-	const setDarkModeData = useSetAtom(darkModeAtom);
+	const [darkModeData, setDarkModeData] = useAtom(darkModeAtom);
 	return (
 		<>
 			<SettingsRow title="Interface" description="Customize your UI theme">
-				<div className="flex gap-2">
-					<button
-						type="button"
+				<div className="flex gap-3">
+					<DarkModeButton
+						label="Light"
+						imgSrc="https://bytebook.nyc3.cdn.digitaloceanspaces.com/color-scheme/light-mode.jpg"
+						imgAlt="light mode"
 						onClick={() =>
 							setDarkModeData({ isDarkModeOn: false, darkModeSetting: "light" })
 						}
-					>
-						Light
-					</button>
-					<button
+						isActive={darkModeData.darkModeSetting === "light"}
+					/>
+					<DarkModeButton
+						label="Dark"
+						imgSrc="https://bytebook.nyc3.cdn.digitaloceanspaces.com/color-scheme/dark-mode.jpg"
+						imgAlt="dark mode"
 						onClick={() =>
 							setDarkModeData({ isDarkModeOn: true, darkModeSetting: "dark" })
 						}
-						type="button"
-					>
-						Dark
-					</button>
-					<button
+						isActive={darkModeData.darkModeSetting === "dark"}
+					/>
+					<DarkModeButton
+						label="System"
+						imgSrc="https://bytebook.nyc3.cdn.digitaloceanspaces.com/color-scheme/light-and-dark-mode.jpg"
+						imgAlt="light and dark mode"
 						onClick={() =>
 							setDarkModeData((prev) => ({
 								...prev,
 								darkModeSetting: "system",
 							}))
 						}
-						type="button"
-					>
-						System
-					</button>
+						isActive={darkModeData.darkModeSetting === "system"}
+					/>
 				</div>
 			</SettingsRow>
 		</>
--- frontend/src/components/settings-dialog/settings-row.tsx
+++ frontend/src/components/settings-dialog/settings-row.tsx
@@ -6,10 +6,10 @@ export function SettingsRow({
 	children,
 }: { title: string; description: string; children: ReactNode }) {
 	return (
-		<div className="grid grid-cols-2 px-0.5 py-2 border-t border-zinc-200 dark:border-zinc-700 ">
-			<div className="flex flex-col gap-1 ">
+		<div className="settings-row-grid px-0.5 py-2.5 border-t border-zinc-200 dark:border-zinc-700 ">
+			<div className="flex flex-col gap-1 h-fit">
 				<h3 className="text-base font-bold">{title}</h3>
-				<p className="text-sm text-zinc-600 dark:text-zinc-400">
+				<p className="text-sm text-zinc-500 dark:text-zinc-400">
 					{description}
 				</p>
 			</div>
