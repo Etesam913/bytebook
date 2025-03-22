09511aecb170e21279ad647d76333e9321d4fc1a
etesam <etesamansari13@gmail.com>
2025-03-22 17:47:59 -0400

    :construction_worker: | sourcemap analyze does not run on every build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
@@ -8,32 +8,42 @@ const ReactCompilerConfig = {
   target: '19',
 };
 
-export default defineConfig(() => ({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
-      },
-    }),
-    tailwindcss(),
-    visualizer({
-      open: true,
-      filename: 'bundle-analysis.html',
-      gzipSize: true,
-      brotliSize: true,
-      template: 'treemap',
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        // This will help identify entry chunks vs lazy-loaded chunks
-        entryFileNames: 'entry-[name].[hash].js',
-        chunkFileNames: 'chunk-[name].[hash].js',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const SHOULD_ANALYZE = env.VITE_ANALYZE === 'true';
+  const visualizerPlugin = SHOULD_ANALYZE
+    ? [
+        visualizer({
+          open: true,
+          filename: 'bundle-analysis.html',
+          gzipSize: true,
+          brotliSize: true,
+          template: 'treemap',
+        }),
+      ]
+    : [];
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+        },
+      }),
+      ...visualizerPlugin,
+      tailwindcss(),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          // This will help identify entry chunks vs lazy-loaded chunks
+          entryFileNames: 'entry-[name].[hash].js',
+          chunkFileNames: 'chunk-[name].[hash].js',
+        },
       },
     },
-  },
-  define: {
-    'process.env.IS_PREACT': JSON.stringify('false'),
-  },
-}));
+    define: {
+      'process.env.IS_PREACT': JSON.stringify('false'),
+    },
+  };
+});
