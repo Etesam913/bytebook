67aadd86a09f2305893f94766fd2faa8d87570a1
Etesam913 <etesamansari13@gmail.com>
2025-09-27 00:12:55 -0400

    💄 | hide extension from sidebar item by default

--- frontend/src/routes/notes-sidebar/sidebar-button/list-note-sidebar-item.tsx
+++ frontend/src/routes/notes-sidebar/sidebar-button/list-note-sidebar-item.tsx
@@ -18,11 +18,16 @@ export function ListNoteSidebarItem({
         fileExtension={sidebarNoteExtension}
         noteNameWithExtension={activeNoteNameWithExtension}
       />
-      <p className="pointer-events-none flex min-w-0">
+      <p className="pointer-events-none flex min-w-0 justify-between w-full">
         <span className="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap">
           {sidebarNoteNameWithoutExtension}
         </span>
-        <span className="shrink-0">.{sidebarNoteExtension}</span>
+
+        {/* 
+        // TODO: Add a setting to enable this 
+        <span className="shrink-0 px-1 py-0.5 outline-2 rounded-md outline-zinc-300 dark:outline-zinc-650 bg-zinc-700 text-xs flex items-center h-fit my-auto">
+          {sidebarNoteExtension}
+        </span> */}
       </p>
     </>
   );
